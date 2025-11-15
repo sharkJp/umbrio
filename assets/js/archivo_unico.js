2cc8a7db388fe2fcdbcc1a970218d341d38dec9f
lolcristian323 <xdv5039@gmail.com>
2025-11-14 22:57:10 -0500

    crear un solo archivo css y un solo js, para evitar tantas llamadas http

---
+++ assets/js/archivo_unico.js
@@ -0,0 +1,8 @@
+import './header.js';
+import './main.js';
+import './seccion1.js';
+import './seccion2.js';
+import './seccion3.js';
+import './seccion4.js';
+import './seccion5.js';
+import './seccion6.js';
